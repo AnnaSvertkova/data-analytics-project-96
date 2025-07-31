f72d13bb1b629b31b77cf84f90eeaa3757068f2d
AnnaSvertkova <svertkovahunter28@mail.ru>
2025-07-31 09:52:34 +0300

    Add files via upload

--- last_paid_click.sql
+++ last_paid_click.sql
@@ -5,42 +5,40 @@ with tab as (
         s.source as utm_source,
         s.medium as utm_medium,
         s.campaign as utm_campaign,
+        l.lead_id,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
         row_number()
-            over (partition by s.visitor_id order by s.visit_date desc)
+            over (
+                partition by s.visitor_id, l.lead_id order by s.visit_date desc
+            )
             as rn
     from sessions as s
+    inner join
+        leads as l
+        on s.visitor_id = l.visitor_id and s.visit_date <= l.created_at
     where s.medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
-),
-
-tab2 as (
-    select
-        visitor_id,
-        visit_date,
-        utm_source,
-        utm_medium,
-        utm_campaign
-    from tab
-    where rn = 1
 )
 
 select
-    t.visitor_id,
-    t.visit_date,
-    t.utm_source,
-    t.utm_medium,
-    t.utm_campaign,
-    l.lead_id,
-    l.created_at,
-    l.amount,
-    l.closing_reason,
-    l.status_id
-from tab2 as t
-inner join
-    leads as l
-    on t.visitor_id = l.visitor_id and t.visit_date <= l.created_at
+    visitor_id,
+    visit_date,
+    utm_source,
+    utm_medium,
+    utm_campaign,
+    lead_id,
+    created_at,
+    amount,
+    closing_reason,
+    status_id
+from tab
+where rn = 1
+
 order by
-    l.amount desc nulls last,
-    t.visit_date asc,
-    t.utm_source asc,
-    t.utm_medium asc,
-    t.utm_campaign asc;
+    amount desc nulls last,
+    visit_date asc,
+    utm_source asc,
+    utm_medium asc,
+    utm_campaign asc;
