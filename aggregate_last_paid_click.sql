15f50e450e7f096123fa5a774fd6169f8c2f727d
AnnaSvertkova <svertkovahunter28@mail.ru>
2025-07-31 21:26:31 +0300

    Add files via upload

--- aggregate_last_paid_click.sql
+++ aggregate_last_paid_click.sql
@@ -1,76 +1,71 @@
-WITH combined_ads AS (
-    SELECT 
-        campaign_date AS visit_date,
-        utm_source AS source,
-        utm_medium AS medium,
-        utm_campaign AS campaign,
-        SUM(daily_spent) AS total_cost
-    FROM (
-        SELECT 
-            campaign_date,
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            daily_spent
-        FROM ya_ads
-        UNION ALL
-        SELECT 
-            campaign_date,
-            utm_source,
-            utm_medium,
-            utm_campaign,
-            daily_spent
-        FROM vk_ads
-    ) AS ads
-    GROUP BY visit_date, source, medium, campaign
-),
-
-last_session_per_visitor AS (
+WITH tab AS (
     SELECT
-        s.visitor_id,
         s.visit_date,
         s.source AS utm_source,
         s.medium AS utm_medium,
         s.campaign AS utm_campaign,
+        COUNT(DISTINCT s.visitor_id) AS visitors_count,
+        COUNT(DISTINCT l.lead_id) AS leads_count,
+        SUM(l.amount) AS revenue,
+        SUM(CASE WHEN l.closing_reason = 'Успешно реализовано' OR l.status_id = 142 THEN 1 ELSE 0 END) AS purchases_count,
         ROW_NUMBER() OVER (PARTITION BY s.visitor_id ORDER BY s.visit_date DESC) AS rn
     FROM sessions AS s
+    INNER JOIN leads AS l
+        ON s.visitor_id = l.visitor_id AND s.visit_date <= l.created_at
     WHERE s.medium IN ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg', 'social')
+    GROUP BY s.visit_date, s.source, s.medium, s.campaign, s.visitor_id
 ),
-
-last_session_data AS (
-    SELECT
-        visitor_id,
-        visit_date,
+tab1 AS (
+    SELECT 
+        campaign_date,
         utm_source,
         utm_medium,
-        utm_campaign
-    FROM last_session_per_visitor
-    WHERE rn = 1
-)
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM ya_ads
+    GROUP BY campaign_date, utm_source, utm_medium, utm_campaign
 
+    UNION ALL
+
+    SELECT 
+        campaign_date,
+        utm_source,
+        utm_medium,
+        utm_campaign,
+        SUM(daily_spent) AS total_cost
+    FROM vk_ads
+    GROUP BY campaign_date, utm_source, utm_medium, utm_campaign
+)
 SELECT
-    TO_CHAR(t4.visit_date, 'YYYY-MM-DD') AS formatted_visit_date,
-    t4.utm_source,
-    t4.utm_medium,
-    t4.utm_campaign,
-    COUNT(t4.visitor_id) AS visitors_count,
-    SUM(tab.total_cost) AS total_cost,
-    COUNT(l.lead_id) AS leads_count,
-    SUM(l.amount) AS revenue,
-    SUM(CASE WHEN l.closing_reason = 'Успешно реализовано' OR l.status_id = 142 THEN 1 ELSE 0 END) AS purchases_count
-FROM last_session_data AS t4
-INNER JOIN leads AS l ON t4.visitor_id = l.visitor_id AND t4.visit_date <= l.created_at
-LEFT JOIN combined_ads AS tab ON 
-    TO_CHAR(t4.visit_date, 'YYYY-MM-DD') = TO_CHAR(tab.visit_date, 'YYYY-MM-DD')
-    AND t4.utm_source = tab.source
-    AND t4.utm_medium = tab.medium
-    AND t4.utm_campaign = tab.campaign
-GROUP BY
-    t4.visit_date, t4.utm_source, t4.utm_medium, t4.utm_campaign
+    TO_CHAR(t.visit_date, 'YYYY-MM-DD') AS visit_date,
+    t.utm_source,
+    t.utm_medium,
+    t.utm_campaign,
+    SUM(t.visitors_count) AS visitors_count,
+    COALESCE(SUM(t1.total_cost), 0) AS total_cost,
+    SUM(t.leads_count) AS leads_count,
+    SUM(t.revenue) AS revenue,
+    SUM(t.purchases_count) AS purchases_count
+FROM tab t
+LEFT JOIN tab1 AS t1
+    ON TO_CHAR(t.visit_date, 'YYYY-MM-DD') = TO_CHAR(t1.campaign_date, 'YYYY-MM-DD')
+    AND t.utm_source = t1.utm_source 
+    AND t.utm_medium = t1.utm_medium
+    AND t.utm_campaign = t1.utm_campaign
+WHERE t.rn = 1
+GROUP BY 
+    TO_CHAR(t.visit_date, 'YYYY-MM-DD'), t.utm_source, t.utm_medium, t.utm_campaign
 ORDER BY
     revenue DESC NULLS LAST,
-    t4.visit_date ASC,
+    visit_date ASC,
     visitors_count DESC,
-    t4.utm_source ASC,
-    t4.utm_medium ASC,
-    t4.utm_campaign ASC;
+    t.utm_source ASC,
+    t.utm_medium ASC,
+    t.utm_campaign ASC;
+
+
+
+
+
+       
+   
